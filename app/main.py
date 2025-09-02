47fd3cd5551fb34d65d18a72e08f756705c9bc1b
lin <lin@de-line.net>
2025-09-02 14:51:53 +0800

    change5001

--- app/main.py
+++ app/main.py
@@ -69,4 +69,4 @@ def static_files(filename):
     return send_from_directory("static", filename)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
